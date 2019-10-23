d8f61e77170418ab8b6aaf167dc2f17ee4034816
Mutalqureshi <35372795+Mutalqureshi@users.noreply.github.com>
2019-10-23 14:21:56 +0500

    Add files via upload

---
+++ php get date.php
@@ -0,0 +1,5 @@
+<label class="dates">	
+	<span class="month"><?php echo get_the_date('M'); ?></span>
+	<span class="date"><?php echo get_the_date('d'); ?></span>
+	<span class="year"><?php echo get_the_date('Y'); ?></span>
+</label>
